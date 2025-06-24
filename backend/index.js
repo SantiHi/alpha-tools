b5bcfb5b217c94ce44075f0291f5917506bb64bf
Santi Criado <criado.santi@gmail.com>
2025-06-24 16:34:18 -0700

    will add signup-page to frontend, got initial signup routes working

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
-const express = require("express");
-app.use(express.json());
-const PORT = process.env.PORT || 3000;
+const { PrismaClient } = require("./generated/prisma");
+const prisma = new PrismaClient();
 
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,15 @@
+const { PrismaClient } = require("../generated/prisma");
+const prisma = new PrismaClient();
+
 const express = require("express");
-app.use(express.json());
 const app = express();
 app.use(express.json());
-router = express.Router();
+const router = express.Router({ mergeParams: true });
 const argon2 = require("argon2");
 
+// Simplistic Signup Route
 router.post("/signup", async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, email, name } = req.body;
   if (!username || !password) {
     return res
       .status(400)
@@ -19,21 +22,35 @@ router.post("/signup", async (req, res) => {
       .json({ error: "Password must be at least 8 characters long." });
   }
 
-  const existingUser = await prisma.user.findUnique({
-    where: { username },
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      OR: [
+        {
+          username,
+        },
+        {
+          email,
+        },
+      ],
+    },
   });
 
   if (existingUser) {
-    return res.status(400).json({ error: "Username already taken." });
+    return res.status(400).json({ error: "Username or email already used." });
   }
-  const hashedPassword = await argon2.hash(password, 10);
+  const hashedPassword = await argon2.hash(password);
 
   const newUser = await prisma.user.create({
     data: {
       username,
-      password,
+      password: hashedPassword,
+      email,
+      name,
     },
   });
 
-  res.status(201).json({ message: `user ${username} created!` });
+  res.status(201).json(newUser);
 });
+
+module.exports = router;
+// Simplistic login Route
