dacd6c05141100a10b37cd52c35ee890d9e7c624
Santi Criado <criado.santi@gmail.com>
2025-07-17 20:43:40 -0700

    added model for technical challenge

--- backend/routes/portfolios.js
+++ backend/routes/portfolios.js
@@ -13,14 +13,16 @@ const router = express.Router({ mergeParams: true });
 require("dotenv").config();
 const finnhub = require("finnhub");
 const finnhubClient = new finnhub.DefaultApi(process.env.finnhubKey);
+const tf = require("@tensorflow/tfjs-node");
 
 const MODE_DAY = "Day";
 const MODE_WEEK = "Week";
 const MODE_MONTH = "Month";
+const THREE_MONTH = "3Months";
 const BAD_PARAMS = "portfolio id is likely incorrect";
 const DOES_NOT_EXIST = "portfolio doesn't exist";
 const PUBLIC_PORTFOLIOS_NUMBER = 6;
-
+const NUM_FEATURES = 4;
 // make new portfolio
 router.post("/", async (req, res) => {
   const userId = req.session.userId;
@@ -236,6 +238,7 @@ router.get("/curated/public", async (req, res) => {
       },
     },
   });
+
   // get companies that are in the portfolios and then score them similar to
   portfolioScores = {};
   for (let portfolio of allPortfolios) {
@@ -316,18 +319,14 @@ router.get("/permissions/user/:id", async (req, res) => {
   res.json({ owner: false, public: portfolio.isPublic });
 });
 
-// Technical challenge #2, preformance prediction
-/* 
+/* Technical challenge #2, preformance prediction, using LSTM model and historical data on 
+each stock to train and then predict the preformance each day for each company
 
-returns two things, stock data in form 
-  [{date: XX, price: price}, ..., ..] 
-
-and also risk indicators in the next month of form 
-  [{data: XX, company: XX, risk XX}]
-  ex of risks: earnings release, 
+This endpoint gets ALL earnings calls in the next month, and puts them in the db so that we have 
+quick access for model. 
 */
 
-router.get("/portfolio/prediction/:id", async (req, res) => {
+router.get("/earnings/:id", async (req, res) => {
   const portfolioid = parseInt(req.params.id);
   const portfolio = await prisma.portfolio.findUnique({
     where: {
@@ -342,11 +341,195 @@ router.get("/portfolio/prediction/:id", async (req, res) => {
   const end = formatDate(nextDate);
   finnhubClient.earningsCalendar(
     { from: begin, to: end },
-    (error, data, response) => {
+    async (error, data, response) => {
       res.json(data);
+      for (let nextEarnings of data.earningsCalendar) {
+        // check that symbol exists in db:
+        const exists = await prisma.company.findUnique({
+          where: {
+            ticker: nextEarnings.symbol,
+          },
+        });
+        if (exists == null) {
+          continue;
+        }
+        await prisma.company.update({
+          where: {
+            ticker: nextEarnings.symbol,
+          },
+          data: {
+            UpcomingEarnings: [
+              nextEarnings.date,
+              String(nextEarnings.epsActual),
+              String(nextEarnings.epsEstimate),
+              String(nextEarnings.revenueActual),
+              String(nextEarnings.revenueEstimate),
+              nextEarnings.symbol,
+            ],
+          },
+        });
+      }
     }
   );
 });
+
+/* create and run model! */
+router.post("/model/:id", async (req, res) => {
+  const FUTURE_DAYS = 30; // how many days we predict **these can be turned into params and specified by users later!"
+  const WINDOW = 40; // how far in the past we look in total
+  const portfolioId = parseInt(req.params.id);
+  const portfolio = await prisma.portfolio.findUnique({
+    where: {
+      id: portfolioId,
+    },
+  });
+  const companyArrays = await prisma.company.findMany({
+    where: {
+      id: {
+        in: portfolio.companiesIds,
+      },
+    },
+  });
+  const tickerArr = companyArrays.map((val) => val.ticker);
+  const todayString = new Date();
+  const earlierString = getBeforeDate(THREE_MONTH);
+  const historicalData = {};
+  for (let tick of tickerArr) {
+    const prices = await yahooFinance.chart(tick, {
+      period1: formatDate(earlierString),
+      period2: formatDate(todayString),
+      interval: "1h",
+    });
+    historicalData[tick] = prices;
+  }
+  // clean data for model! we will add each of the values together and then get out
+  // we will start by adding together each value and then later normalizing
+  let cleanData = []; // earliest dates are earliest in the index!
+  let dates = [];
+  let labels = [];
+  let highArr = [];
+  let volArr = [];
+  let openArr = [];
+  let lowArr = [];
+  let adjArr = [];
+  for (let i = 0; i < historicalData[tickerArr[0]].quotes.length; i++) {
+    let concatData = new Array(NUM_FEATURES).fill(0);
+    let labelVal = 0;
+    dates[i] = historicalData[tickerArr[0]].quotes[i].date; // dates should be the same everywhere
+    for (let tick of tickerArr) {
+      if (historicalData[tick].quotes[i] == null) {
+        break;
+      }
+      // double for loops generally bad practice, but tickerArr is constant size (I doubt a portfolio will exceed 100 companies!)
+      concatData[0] += historicalData[tick].quotes[i].high;
+      concatData[1] += historicalData[tick].quotes[i].volume;
+      concatData[2] += historicalData[tick].quotes[i].open;
+      concatData[3] += historicalData[tick].quotes[i].low;
+      labelVal += historicalData[tick].quotes[i].close;
+    }
+    cleanData.push(concatData);
+    highArr.push(concatData[0]);
+    volArr.push(concatData[1]);
+    openArr.push(concatData[2]);
+    lowArr.push(concatData[3]);
+    adjArr.push(concatData[4]);
+    labels.push(labelVal);
+  }
+  normalize(
+    cleanData,
+    [
+      Math.min(...highArr),
+      Math.min(...volArr),
+      Math.min(...openArr),
+      Math.min(...lowArr),
+    ],
+    [
+      Math.max(...highArr),
+      Math.max(...volArr),
+      Math.max(...openArr),
+      Math.max(...lowArr),
+    ],
+    true
+  );
+  let maxLabel = [Math.max(...labels)];
+  let lastPrice = labels[labels.length - 1];
+  let minLabel = [Math.min(...labels)];
+  normalize(labels, minLabel, maxLabel, false);
+
+  // data is now cleaned, so we create vectors for our model
+
+  const X = []; // shape is ( #datapoints X WINDOW x NUM_FEATURES) - 80 past datapoints (can add more if needed), we have WINDOW inputs (past cost) and NUM_FEATURES features each (high, volume, open, low, adjclose)
+  const Y = []; // this is size (1 x FUTURE_DAYS)
+  for (let i = 0; i + WINDOW + FUTURE_DAYS <= cleanData.length; i++) {
+    X.push(cleanData.slice(i, i + WINDOW));
+    Y.push(labels.slice(i + WINDOW, i + WINDOW + FUTURE_DAYS));
+  }
+  const X_values = tf.tensor3d(X, [X.length, WINDOW, 4]);
+  const Y_values = tf.tensor2d(Y, [Y.length, FUTURE_DAYS]);
+  // model inspired by https://codelabs.developers.google.com/codelabs/tfjs-training-regression/index.html#8,
+  // but heavily modified with own data and using lstm instead of only dense
+  const model = tf.sequential();
+  model.add(
+    tf.layers.lstm({
+      units: 50,
+      inputShape: [WINDOW, NUM_FEATURES],
+      returnSequences: false,
+    })
+  );
+  model.add(tf.layers.dense({ units: FUTURE_DAYS, activation: "tanh" }));
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: tf.losses.meanSquaredError,
+    metrics: ["mse"],
+  });
+  await model.fit(X_values, Y_values, {
+    epochs: 10,
+    batchSize: 32,
+    validationSplit: 0.2,
+    verbose: 1,
+  });
+
+  const lastDays = cleanData.slice(-WINDOW);
+  const input = tf.tensor3d([lastDays], [1, WINDOW, NUM_FEATURES]);
+  const prediction = model.predict(input);
+  const predictionArray = (await prediction.array())[0];
+  let currentDate = new Date();
+  let offset =
+    lastPrice - parseFloat(unNormalize(predictionArray[0], minLabel, maxLabel));
+  const valuePredict = predictionArray.map((value) => {
+    lastPrice = unNormalize(value, minLabel, maxLabel);
+    currentDate.setDate(currentDate.getDate() + 1);
+    return {
+      date: formatDate(currentDate),
+      price: parseFloat(lastPrice) + offset,
+    };
+  });
+  tf.dispose([X_values, Y_values, input, prediction]);
+  res.json(valuePredict);
+});
+
+// normalize data using xi - min(x) / (max(x) - min(x)) to get data with mean=0 and std 1,
+// normalizes along columns
+const normalize = (arrToNormalize, minvalues, maxvalues, double) => {
+  if (double) {
+    for (let i = 0; i < arrToNormalize.length; i++) {
+      for (let k = 0; k < 4; k++) {
+        arrToNormalize[i][k] =
+          (arrToNormalize[i][k] - minvalues[k]) / (maxvalues[k] - minvalues[k]);
+      }
+    }
+    return arrToNormalize;
+  }
+  for (let i = 0; i < arrToNormalize.length; i++) {
+    arrToNormalize[i] =
+      (arrToNormalize[i] - minvalues[0]) / (maxvalues[0] - minvalues[0]);
+  }
+  return arrToNormalize;
+};
+
+const unNormalize = (value, min, max) => {
+  return value * (max - min) + min;
+};
 
 const formatDate = (dateObj) => {
   const formattedDate = `${dateObj.getFullYear()}-${String(
@@ -365,6 +548,8 @@ const getBeforeDate = (timeFrame) => {
     prevDate.setDate(prevDate.getDate() - 7);
   } else if (timeFrame === MODE_MONTH) {
     prevDate.setMonth(prevDate.getMonth() - 1);
+  } else if (timeFrame === THREE_MONTH) {
+    prevDate.setMonth(prevDate.getMonth() - 3);
   } else {
     prevDate.setFullYear(prevDate.getFullYear() - 1);
   }
